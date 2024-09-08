3446448ae78ac9e96d5ac16925f69c577fe10575
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-09-08 15:09:33 +0800

    added github URL footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App </h1>
+      <footer>
+        {" This project was coded by Cheryl Watts and is "}
+        <a
+          href="https://github.com/chezzer84/react-week5-weatherapp"
+          target="blank"
+        >
+          open sourced
+        </a>
+      </footer>
+    </div>
+  );
 }
