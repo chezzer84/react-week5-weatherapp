6dee04a7605015736ac28c2a365bd4eb7c55486e
chezzer84 <115329113+chezzer84@users.noreply.github.com>
2024-09-08 15:17:20 +0800

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App </h1>
-      <footer>
-        {" This project was coded by Cheryl Watts and is "}
-        <a
-          href="https://github.com/chezzer84/react-week5-weatherapp"
-          target="blank"
-        >
-          open sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App </h1>
+        <footer>
+          {" This project was coded by Cheryl Watts and is "}
+          <a
+            href="https://github.com/chezzer84/react-week5-weatherapp"
+            target="blank"
+          >
+            open sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
